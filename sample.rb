4536fcb43064cf3964835080e754a7bf00ee23db
KokichiOmi <36585327+KokichiOmi@users.noreply.github.com>
2018-02-25 21:51:30 +0900

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -1,4 +1,4 @@
-# FizzBuzzの判定条件分岐
+# FizzBuzz判定の条件分岐
 def FizzBuzz(number)
     if number % 3 == 0 && number % 5 == 0
         puts 'FizzBuzz'
@@ -11,16 +11,16 @@ def FizzBuzz(number)
     end
 end
 
-# 入力値の精査と、数値型の際の判定式の表示
+# 入力値の受け取りと、数字型のみ条件式から判定表示
 while true
     puts '任意の数字を入力してください！'
     number = gets.chomp
-    if number =~ /^[0-9]+$/
+    if number =~ /^[1-9][0-9]*$/
         number = number.to_i
         output = FizzBuzz(number)
         puts "#{output}"
-        break
     else
         puts "半角数字で入力してください"
+        break
     end
 end
